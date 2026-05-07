6f4df0fa41f8068146675d184978d9bb7a6ba30b
Frans Filasta Pratama <fransfilastap@live.com>
2026-05-08 00:44:41 +0700

    feat(visitor-counter): add menu migration for visitor report

---
+++ console/migrations/_next/m260507_000003_insert_visitor_report_menu.php
@@ -0,0 +1,22 @@
+<?php
+namespace _next;
+
+use yii\db\Migration;
+
+class m260507_000003_insert_visitor_report_menu extends Migration
+{
+    public function safeUp()
+    {
+        $this->insert('{{%menu}}', [
+            'name' => 'Statistik Pengunjung',
+            'route' => '/visitor-report/index',
+            'data' => json_encode(['icon' => 'chart-bar']),
+            'order' => 999,
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->delete('{{%menu}}', ['route' => '/visitor-report/index']);
+    }
+}
