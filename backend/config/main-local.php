24d9ed27c02d7e9587ee14ebcaca9f7bcc23e32e
Haura Taqiya Azza Nabila <hauranabila2244@gmail.com>
2026-07-10 15:01:04 +0700

    otw ganti postgre

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -1,8 +1,23 @@
 <?php
-return [
+
+$config = [
     'components' => [
         'request' => [
             'cookieValidationKey' => getenv('COOKIE_VALIDATION_KEY_BE'),
         ],
     ],
 ];
+
+if (YII_ENV_DEV) {
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
